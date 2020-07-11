57ba1cc5dc0f21a0966022dee78a2ec9ab88010b
Akinsiku Femi <akinsiku13@gmail.com>
2020-07-11 11:23:55 +0100

    Add code to render assets in production due to bg image fail in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,3 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+#config/production.rb
+APPLICATIONNAME::Application.configure do
+     config.assets.compile = true
+end
